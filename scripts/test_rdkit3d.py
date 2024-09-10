29b62b36140cf40feef66f685a962ed407294b7e
alexander.korolyov <korol.al@gmail.com>
2024-09-10 08:33:23 +0200

    update

---
+++ scripts/test_rdkit3d.py
@@ -0,0 +1,47 @@
+import multiprocessing
+
+import pandas as pd
+import datamol as dm
+import numpy as np
+import seaborn as sns
+
+from molfeat.calc import RDKitDescriptors2D, FPCalculator, MordredDescriptors
+from molfeat.trans import MoleculeTransformer, FPVecTransformer
+from sklearn.preprocessing import OneHotEncoder
+
+import collections.abc as collections
+from molfeat.trans.concat import FeatConcat
+
+from sklearn.ensemble import HistGradientBoostingClassifier, RandomForestClassifier
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.impute import SimpleImputer
+
+from src.config import mem
+from src.utils import embed3d, eval_model, OffsetScaler
+from src.descriptors import get_dgl_predictions, get_hft_predictions, get_3d_predictions, get_2d_predictions
+
+from rdkit import RDLogger, Chem
+RDLogger.DisableLog('rdApp.*')
+
+if __name__ == '__main__':
+    train = dm.read_csv('../data/processed/train.csv', smiles_column='smi', index_col=0)[:5]
+    test = dm.read_csv('../data/processed/test.csv', smiles_column='smi', index_col=0)[:5]
+
+    n_confs = 3
+    mols = mem.cache(embed3d, ignore=['n_jobs'])(train.smi[0:1], n_confs=n_confs, n_jobs=1)
+    trans = FPVecTransformer('desc3D', length=639, n_jobs=1)
+
+    print('trans.transform(mols, ...)')
+    # print(trans.transform(mols, conformer_id=0, ignore_errors=False)[0, :5].round(3))
+    print(trans.transform(mols, conformer_id=1, ignore_errors=False)[0, :5].round(3))
+
+    print('trans(mols, ... ')
+    # print(trans(mols, conformer_id=0, ignore_errors=False)[0, :5].round(3))
+    print(trans(mols, conformer_id=1, ignore_errors=False)[0, :5].round(3))
+
+    from molfeat.calc.descriptors import RDKitDescriptors3D
+
+    rd3d = RDKitDescriptors3D()
+    print('rd3d = RDKitDescriptors3D()')
+    # print(rd3d(mols[0], conformer_id=0)[:5].round(3))
+    print(rd3d(mols[0], conformer_id=1)[:5].round(3))
