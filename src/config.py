9404ad7209e97aa41637b3abeda765853430c491
alkorolyov <korol.al@gmail.com>
2024-09-16 11:49:12 +0200

    update

--- src/config.py
+++ src/config.py
@@ -8,3 +8,53 @@ warnings.filterwarnings("ignore")
 mem = Memory(location='../data/.cache')
 
 RANDOM_SEED = 42
+
+feats2D_params = [
+    # fps
+    {'kind': 'ecfp:4', 'length': 1024},
+    {'kind': 'topological', 'length': 1024, 'n_jobs': -1},
+    {'kind': 'avalon', 'length': 1024, 'n_jobs': -1},
+    {'kind': 'layered', 'length': 1024, 'n_jobs': -1},
+    {'kind': 'secfp', 'length': 1024, 'n_jobs': -1},
+    {'kind': 'pattern', 'length': 1024},
+    {'kind': 'pharm2D', 'length': 1024, 'n_jobs': -1},
+
+    # fixed length fps
+    {'kind': 'erg', 'n_jobs': -1},
+    {'kind': 'maccs', 'n_jobs': -1},
+
+    # fixed length descriptors - to normalize
+    {'kind': 'estate', 'n_jobs': -1},
+    {'kind': 'desc2D', 'n_jobs': -1},
+    {'kind': 'mordred', 'n_jobs': -1},
+    {'kind': 'cats2D', 'n_jobs': -1},
+    {'kind': 'scaffoldkeys', 'n_jobs': -1},
+    {'kind': 'skeys', 'n_jobs': -1},
+]
+
+feats3D_params = [
+    {'kind': 'pharm3D', 'length': 1024, 'n_jobs': -1},
+    {'kind': 'desc3D', 'n_jobs': -1},
+    {'kind': 'cats3D', 'n_jobs': -1},
+    {'kind': 'electroshape', 'n_jobs': -1},
+    {'kind': 'usr', },
+    {'kind': 'usrcat', },
+]
+
+dgl_params = [
+    {'kind': 'gin_supervised_contextpred'},
+    {'kind': 'gin_supervised_infomax'},
+    {'kind': 'gin_supervised_edgepred'},
+    {'kind': 'gin_supervised_masking'},
+]
+
+hf_params = [
+    # {'kind': 'ChemBERTa-77M-MLM', 'random_seed': 42},
+    {'kind': 'ChemBERTa-77M-MTR', 'random_seed': 42},
+    # {'kind': 'ChemGPT-1.2B', 'random_seed': 42},
+    {'kind': 'ChemGPT-19M', 'random_seed': 42},
+    {'kind': 'ChemGPT-4.7M', 'random_seed': 42},
+    {'kind': 'GPT2-Zinc480M-87M', 'random_seed': 42},
+    # {'kind': 'MolT5', 'random_seed': 42},
+    {'kind': 'Roberta-Zinc480M-102M', 'random_seed': 42},
+]
--- src/representation.py
+++ src/representation.py
@@ -1,8 +1,11 @@
+from tqdm import tqdm
 import pandas as pd
+import numpy as np
 
-from src.utils import smi2mol
+from src.config import feats2D_params, feats3D_params, dgl_params, mem
+from src.utils import smi2mol, embed3d
 from src.fingerprints import get_fingerprints
-from src.descriptors import get_rd_descriptors, get_md_descriptors
+from src.descriptors import get_rd_descriptors, get_md_descriptors, calc_2d_feats, calc_3d_feats, calc_dgl_feats
 
 from joblib import Parallel, delayed
 from rdkit import RDLogger
@@ -53,3 +56,43 @@ def get_representation_from_series(smiles: pd.Series, config=None, n_jobs=1) -> 
         return pd.DataFrame(res, index=smiles.index)
     else:
         raise ValueError('n_jobs must be 1 or greater.')
+
+
+def calc_all_features(smiles, inclue_3D=False):
+    def get_length(arr):
+        for a in arr:
+            if a is not None:
+                return a.shape[0]
+
+    def feats_np_to_df(arr, params):
+        length = get_length(arr)
+        nan_arr = np.array([np.nan] * length)
+        arr = [nan_arr if a is None else a for a in arr]  # fill None with numpy nan array
+        arr = np.stack(arr)
+        cols = [params['kind'] + '_' + str(i) for i in range(length)]
+        return pd.DataFrame(arr, columns=cols, index=smiles.index)
+
+    df = pd.DataFrame(index=smiles.index)
+
+    for params in tqdm(feats2D_params, desc='feats2D'):
+        arr = mem.cache(calc_2d_feats, ignore=['n_jobs'], verbose=False)(smiles, **params)
+        # print(params['kind'], arr[1].shape)
+        feats_df = feats_np_to_df(arr, params)
+        df = pd.concat([df, feats_df], axis=1)
+
+    if inclue_3D:
+        mem.cache(embed3d)(smiles)
+        for params in tqdm(feats3D_params, desc='feats3D'):
+            arr = mem.cache(calc_3d_feats, verbose=False)(smiles, **params)
+            # print(params['kind'], arr[1].shape)
+            feats_df = feats_np_to_df(arr, params)
+            df = pd.concat([df, feats_df], axis=1)
+
+    for params in tqdm(dgl_params, desc='dgl'):
+        try:
+            arr = mem.cache(calc_dgl_feats, verbose=False)(smiles, **params)
+            feats_df = feats_np_to_df(arr, params)
+            df = pd.concat([df, feats_df], axis=1)
+        except:
+            pass
+    return df
